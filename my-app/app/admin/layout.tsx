824a8d2c86bcf09570edd91bb945a4adea164653
kush825 <parmarkush0089@gmail.com>
2026-01-20 08:09:29 +0530

    admin layout changes

--- my-app/app/admin/layout.tsx
+++ my-app/app/admin/layout.tsx
@@ -3,18 +3,16 @@ import { isAdmin } from '@/actions/admin'
 import { redirect } from 'next/navigation'
 import AdminSidebar from '@/components/admin/AdminSidebar'
 
+
 export default async function AdminLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
     // Client-side protection is good, but server-side is better
-    if (!await isAdmin()) {
+   if (!await isAdmin()) {
         redirect('/dashboard')
     }
-
-
-
     return (
         <div className="flex flex-col md:flex-row h-screen bg-slate-950 text-slate-100 font-sans overflow-hidden">
             <AdminSidebar />
